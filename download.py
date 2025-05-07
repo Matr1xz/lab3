bbf4af28aa7b3f8a7ad10c9963f28690c53b789d
Mai Xuan Nhat <143202719+Matr1xz@users.noreply.github.com>
2025-05-07 17:37:53 +0700

    Update download.py

--- download.py
+++ download.py
@@ -1,9 +1,9 @@
 import os
 import time
 
-url = "http://172.17.0.3:8000/video.zip"
+url = "http://172.17.0.2:8000/video.zip"
 
 for i in range(2):
-    print(f"Download lần {i+1}...")
+   
     os.system(f"wget -O video.zip {url}")
-    time.sleep(2)  # chờ 2 giây giữa 2 lần tải (tuỳ chọn)
+    time.sleep(2)  
